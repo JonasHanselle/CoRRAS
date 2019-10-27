c151c9b3948739c7e6ad62fc904a040d2e0ea11b
Jonas Hanselle <jmhansel@mail.uni-paderborn.de>
2019-10-27 22:10:27 +0100

    Work on log-linear model, tests, asranking scenario

--- Corras/Model/Tests/log_linear_test.py
+++ Corras/Model/Tests/log_linear_test.py
@@ -1,24 +1,29 @@
 import unittest
 import Corras.Scenario.aslib_ranking_scenario as scen
 import Corras.Model.log_linear as ll
-from sklearn.preprocessing import StandardScaler
 
 class TestLogLinearModel(unittest.TestCase):
 
     def test_fit(self):
-        test_scen = scen.ASRankingScenario()
-        test_scen.read_scenario("aslib_data-aslib-v4.0/CSP-2010")
-        test_scen.compute_rankings()
-        model = ll.LogLinearModel()
-        model.fit(test_scen)
-        scaler = StandardScaler()
-        features_scaled = scaler.fit_transform(test_scen.feature_data)
-        feature_vector = test_scen.feature_data.iloc[15].values
-        feature_vector_scaled = features_scaled[15]
-        print(feature_vector)
-        print(feature_vector_scaled)
-        model.predict(feature_vector)
-        model.predict(feature_vector_scaled)
+        scenario = scen.ASRankingScenario()
+        scenario.read_scenario("aslib_data-aslib-v4.0/CPMP-2015")
+        scenario.compute_rankings()
+        print("length before", len(scenario.performance_rankings))
+        scenario.remove_duplicates()
+        print("length after", len(scenario.performance_rankings))
+        # test_scen, train_scen = scenario.get_split(indx=5)
+        # model = ll.LogLinearModel()
+
+        # model.fit(train_scen)
+
+        # for index in range(1,10):
+        #     split = scenario.get_split(indx=index)
+        #     train_scenario = split[0]
+        #     test_scenario = split[1]
+        #     train_scenario.performance_rankings.to_csv("train_ranking_" + str(index) + ".csv")
+        #     train_scenario.performance_data.to_csv("train_performance_" + str(index) + ".csv")
+        #     test_scenario.performance_rankings.to_csv("test_ranking_" + str(index) + ".csv")
+        #     test_scenario.performance_data.to_csv("test_performance_" + str(index) + ".csv")
         assert(True)
 
 if __name__ == "__main__":
--- Corras/Model/log_linear.py
+++ Corras/Model/log_linear.py
@@ -2,42 +2,89 @@ import numpy as np
 import pandas as pd
 from Corras.Scenario.aslib_ranking_scenario import ASRankingScenario
 
+
 class PLNegativeLogLikelihood:
-    
-    def __init__(self, dataset=None, ):
+
+    def __init__(self):
         pass
 
-    def negative_log_likelihood(self):
-        return None
+    def negative_log_likelihood(self, rankings: pd.DataFrame, features: pd.DataFrame, weights: np.ndarray):
+        """Compute NLL w.r.t. the data in the given batch and the given weights
 
-    def first_derivative(self):
+        Arguments:
+            rankings {pandas.DataFrame} -- Data sample for computing the NNL
+            features {pandas.DataFrame} -- Feature values for computing the NNL
+            weights {np.ndarray} -- Weight vector, i.e. model parameters
+
+        Returns:
+            [float64] -- Negative log-likelihood
+        """
+        outer_sum = 0
+        for index, ranking in rankings.iterrows():
+            # print("index", index)
+            feature_values = features.loc[index].values
+            # print("feature values",  feature_values)
+            inner_sum = 0
+            for m in range(0,len(ranking)):
+                remaining_sum = 0
+                for j in range(m,len(ranking)):
+                    # compute utility of remaining labels
+                    remaining_sum += np.exp(np.dot(weights[ranking[j]-1],feature_values))
+                inner_sum += np.log(np.exp(np.dot(weights[ranking[m]-1],feature_values))) - np.log(remaining_sum)
+            outer_sum += inner_sum
+        return -outer_sum               
+        
+    def first_derivative(self, rankings: pd.DataFrame, features: pd.DataFrame, weights: np.ndarray):
+        """Computes the first gradient 
+        
+        Arguments:
+            rankings {pd.DataFrame} -- [description]
+            features {pd.DataFrame} -- [description]
+            weights {np.ndarray} -- [description]
+        
+        Returns:
+            [type] -- [description]
+        """
         return None
 
     def second_derivative(self):
         return None
 
+
 class LogLinearModel:
 
-    def __init__(self, regression_penalty="absolute"):
+    def __init__(self):
         self.weights = None
+        self.dataset = None
 
-    def fit(self, data : ASRankingScenario):
+    def fit(self, dataset: ASRankingScenario):
         """Fits a label ranking model based on a log-linear utility function.
-        
+
         Arguments:
             data {ASRankingScenario} -- Training data set
         """
-        num_labels = len(data.algorithms)
-        num_features = len(data.features)
-        self.weights = np.ones(shape=(num_features,num_labels))
-        print(self.weights)
+        self.dataset = dataset
+        num_labels = len(dataset.algorithms)
+        num_features = len(dataset.features)
+        self.weights = np.ones(shape=(num_labels, num_features))
 
-    def predict(self, features : np.ndarray):
+        # Start gradient descent
+        minibatch_size = 1
+        learning_rate = 0.1
+        max_iter = 1
+        nll = PLNegativeLogLikelihood()
+        for i in range(0, max_iter):
+            print(i)
+            minibatch = dataset.performance_rankings.sample(minibatch_size)
+            nll_value = nll.negative_log_likelihood(minibatch, dataset.feature_data, self.weights)
+            print("nll", nll_value)
+
+    def predict(self, features: np.ndarray):
         """Predict a label ranking.
-        
+
         Arguments:
             features {np.ndarray} -- Instance feature values
-        
+
         Returns:
             pd.DataFrame -- Ranking of algorithms
         """
