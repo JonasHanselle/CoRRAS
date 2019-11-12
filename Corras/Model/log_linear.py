Fix bug in nll, work on vectorized computation

Corras/Model/log_linear.py
@@ -22,8 +22,8 @@ class LogLinearModel:
             [type] -- [description]
         """
         loss = 0
-        for row in performances.itertuples():
-            current_performances = row
+        for row in performances.iterrows():
+            current_performances = row.values
             feature_values = np.hstack((features.loc[index].values, [1]))
             utilities = np.exp(np.dot(weights, feature_values))
             inverse_utilities = np.reciprocal(utilities)
@@ -42,8 +42,8 @@ class LogLinearModel:
             [type] -- [description]
         """
         loss = 0
-        for index, row in performances.itertuples():
-            current_performances = row
+        for index, row in performances.iterrows():
+            current_performances = row.values
             feature_values = np.hstack((features.loc[index].values, [1]))
             utilities = np.exp(np.dot(weights, feature_values))
             inverse_utilities = np.reciprocal(utilities)
@@ -63,11 +63,11 @@ class LogLinearModel:
             [float64] -- Negative log-likelihood
         """
         outer_sum = 0
-        for index, ranking in rankings.itertuples():
+        for index, ranking in rankings.iterrows():
             # add one column for bias
             feature_values = np.hstack((features.loc[index].values, [1]))
             inner_sum = 0
-            # ranking = ranking.values
+            ranking = np.argsort(ranking.values)
             for m in range(0, len(ranking)):
             # if ranking[m] > 0:
                 remaining_sum = 0
@@ -75,16 +75,48 @@ class LogLinearModel:
                     # compute utility of remaining labels
                     # if ranking[j] > 0:
                         remaining_sum += np.exp(
-                            np.dot(weights[ranking[j]-1], feature_values))
+                            np.dot(weights[ranking[j]], feature_values))
                 inner_sum += np.log(remaining_sum) - \
-                    np.dot(weights[ranking[m]-1], feature_values)
+                    np.dot(weights[ranking[m]], feature_values)
             outer_sum += inner_sum
         return outer_sum
 
-    def fast_nll(self, weights, features, orderings):
+    def vectorized_nll(self, rankings, features, weights):
+        """Compute NLL w.r.t. the data in the given batch and the given weights
+
+        Arguments:
+            rankings {np.ndarray} -- Data sample for computing the NNL
+            features {np.ndarray} -- Feature values for computing the NNL
+            weights {np.ndarray} -- Weight vector, i.e. model parameters
+
+        Returns:
+            [float64] -- Negative log-likelihood
+        """
         nll = 0
-        # utilities = 
-        return nll
+        # print("num_features", features.shape[1], "num_labels", rankings.shape[1], "weights", weights.shape)
+        # print("features",features)
+        # print("rankings",rankings)
+        # print("weights",weights)
+        features = np.hstack((features,np.ones((features.shape[0],1))))
+        # print(features)
+        ordered_weights = weights.T[np.argsort(rankings)]
+        print("weights", weights)
+        print("rankings", rankings)
+        print("ordered weights", ordered_weights)
+        print("features", features)
+        # print(ordered_weights)
+        weighted_features = np.tensordot(ordered_weights, features.T, axes=(2,0))
+        
+        print("weighted_features", weighted_features)
+        sum1 = np.sum(np.sum(weighted_features))
+        utilities = np.exp(weighted_features)
+        print("utilities", utilities)
+        logs = np.zeros([rankings.shape[0], rankings.shape[1]])
+        for m in range(0,rankings.shape[1]):
+            logs[:,m] = np.log(np.sum(utilities[:,m:]))
+        sum2 = np.sum(np.sum(logs))
+        nll = sum1 - sum2
+        return -nll
 
     def fit(self, rankings: pd.DataFrame, inverse_rankings: pd.DataFrame, features: pd.DataFrame, performances: pd.DataFrame, lambda_value=0.5, regression_loss="Absolute", maxiter=1000):
         """[summary]
@@ -123,7 +155,51 @@ class LogLinearModel:
 
         flat_weights = self.weights.flatten()
         result = minimize(f, flat_weights, method="L-BFGS-B",
-                          jac=jac, options={"maxiter": maxiter, "disp": False})
+                          jac=jac, options={"maxiter": maxiter, "disp": True})
+
+        print("Result", result)
+        self.weights = np.reshape(result.x, (num_labels, num_features))
+        print("Weights", self.weights)
+
+
+    def fit_np(self, rankings, inverse_rankings, features, performances, lambda_value=0.5, regression_loss="Absolute", maxiter=1000):
+        """[summary]
+
+        Arguments:
+            rankings {np.ndarray} -- [description]
+            inverse_rankings {np.ndarray} -- [description]
+            features {np.ndarray} -- [description]
+
+        Returns:
+            [type] -- [description]
+        """
+        num_labels = rankings.shape[1]
+        # add one column for bias
+        num_features = features.shape[1]+1
+        print("labels", num_labels, "features", num_features)
+        self.weights = np.ones(
+            shape=(num_labels, num_features)) / num_features * num_labels
+        nll = self.vectorized_nll
+        reg_loss = None
+        if regression_loss == "Absolute":
+            reg_loss = self.absolute_error
+        elif regression_loss == "Squared":
+            reg_loss = self.squared_error
+
+        # minimize loss function
+        def f(x):
+            x = np.reshape(x, (num_labels, num_features))
+            if lambda_value == 0:
+                return reg_loss(performances, features, x)
+            elif lambda_value == 1:
+                return nll(rankings, features, x)
+            return lambda_value * nll(rankings, features, x) + (1 - lambda_value) * reg_loss(performances, features, x)
+
+        jac = grad(f)
+
+        flat_weights = self.weights.flatten()
+        result = minimize(f, flat_weights, method="L-BFGS-B",
+                          jac=None, options={"maxiter": maxiter, "disp": True})
 
         print("Result", result)
         self.weights = np.reshape(result.x, (num_labels, num_features))

label_ranking.py
@@ -6,7 +6,6 @@ from scipy.stats import kendalltau
 import matplotlib.pyplot as plt
 import seaborn as sb
 
-
 df = pd.read_table(sep="\t",filepath_or_buffer="LabelRankingData/iris_dense.txt")
 df = df.iloc[1:]
 feature_columns = [x for x in df.columns if x[0]=="A"]
@@ -16,7 +15,7 @@ rankings = df[ranking_columns]
 print(features)
 print(rankings)
 
-kf = KFold(n_splits=10 , shuffle=True, random_state=5)
+kf = KFold(n_splits=2 , shuffle=True, random_state=5)
 split = next(kf.split(df), None)
 
 # training_portions = np.linspace(start=0, stop=1, num=5)
@@ -26,26 +25,36 @@ result_data = []
 for split_num, split in enumerate(kf.split(df)):
 
     for portion in training_portions:
+        
+        
+        
         train_features = features.iloc[split[0]].astype('float64')
         train_rankings = rankings.iloc[split[0]].astype('int32')
         test_features = features.iloc[split[1]].astype('float64')
         test_rankings = rankings.iloc[split[1]].astype('int32')
 
-
         train_features = train_features[:int(portion*len(train_features))] 
         train_rankings = train_rankings[:int(portion*len(train_rankings))]
 
+        train_features_np = train_features[:int(portion*len(train_features))].values
+        train_rankings_np = train_rankings[:int(portion*len(train_rankings))].values
+
+        # train_rankings_np = np.asarray([[1,2,3],[2,1,3],[3,2,1]])
+        # train_features_np = train_features_np[:3]
+        
         if(len(train_rankings) == 0):
             continue
 
         print("len", len(train_features), len(train_rankings))
 
         model = log_linear.LogLinearModel()
-        model.fit(train_rankings,None,train_features,None,lambda_value=1,regression_loss="Squared", maxiter=10000)
-
+        # model.fit(train_rankings,None,train_features,None,lambda_value=1,regression_loss="Squared", maxiter=100)
+        model.fit_np(train_rankings_np,None,train_features_np,None,lambda_value=1,regression_loss="Squared", maxiter=100)
+        # nll = model.vectorized_nll(train_rankings_np,train_features_np,np.random.rand(train_features_np.shape[1]+1,train_rankings_np.shape[1]))
+        # print("nll", nll)
         current_taus = []
 
-        for index, row in test_features.itertuples():
+        for index, row in test_features.iterrows():
             predicted_ranking = model.predict_ranking(row)
             true_ranking = test_rankings.loc[index].values
             tau = kendalltau(predicted_ranking,true_ranking).correlation