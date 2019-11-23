7131ac5aa6fcd298ed2ed69bd8f053298a46e727
Jonas Hanselle <jmhansel@mail.uni-paderborn.de>
2019-11-23 22:15:23 +0100

    Work on nn

--- Corras/Model/Tests/neural_network_test_synthetic.py
+++ Corras/Model/Tests/neural_network_test_synthetic.py
@@ -20,11 +20,12 @@ class TestNeuralNetworkSynthetic(unittest.TestCase):
             for features in feature_list:
                 # generate performances as functions linear in the features
                 performance_1 = 5 * features[0] + 2 * features[1] + 7 * features[2] + 42
-                performance_2 =  3 * features[1] + 5 * features[3] + 14
-                performance_3 = 2 * features[0] + 4 * features[1] + 11 * features[3] + 77
-                performance_4 = 7 * features[1] + 4 * features[0] + 11 * features[2] + features[3]
-                performance_5 = 2 * features[1] + 9 * features[2] + 7 * features[3] + 12 + features[0]
-                performances.append([performance_1, performance_2, performance_3, performance_4, performance_5])
+                # performance_2 =  3 * features[1] + 5 * features[3] + 14
+                # performance_3 = 2 * features[0] + 4 * features[1] + 11 * features[3] + 77
+                # performance_4 = 7 * features[1] + 4 * features[0] + 11 * features[2] + features[3]
+                # performance_5 = 2 * features[1] + 9 * features[2] + 7 * features[3] + 12 + features[0]
+                # performances.append([performance_1, performance_2, performance_3, performance_4, performance_5])
+                performances.append([performance_1])
             return performances
 
         performances_train = np.asarray(create_performances(features_train), dtype=np.float64)
@@ -42,10 +43,14 @@ class TestNeuralNetworkSynthetic(unittest.TestCase):
 
         self.train_inst = pd.DataFrame(data=features_train,columns=["a","b","c","d"])
         self.test_inst = pd.DataFrame(data=features_test,columns=["a","b","c","d"])
-        self.train_performances = pd.DataFrame(data=performances_train,columns=["alg1","alg2","alg3","alg4","alg5"])
-        self.test_performances = pd.DataFrame(data=performances_test,columns=["alg1","alg2","alg3","alg4","alg5"])
-        self.train_ranking = pd.DataFrame(data=rankings_train,columns=["alg1","alg2","alg3","alg4","alg5"])
-        self.test_ranking = pd.DataFrame(data=rankings_test,columns=["alg1","alg2","alg3","alg4","alg5"])
+        # self.train_performances = pd.DataFrame(data=performances_train,columns=["alg1","alg2","alg3","alg4","alg5"])
+        # self.test_performances = pd.DataFrame(data=performances_test,columns=["alg1","alg2","alg3","alg4","alg5"])
+        # self.train_ranking = pd.DataFrame(data=rankings_train,columns=["alg1","alg2","alg3","alg4","alg5"])
+        # self.test_ranking = pd.DataFrame(data=rankings_test,columns=["alg1","alg2","alg3","alg4","alg5"])
+        self.train_performances = pd.DataFrame(data=performances_train,columns=["alg1"])
+        self.test_performances = pd.DataFrame(data=performances_test,columns=["alg1"])
+        self.train_ranking = pd.DataFrame(data=rankings_train,columns=["alg1"])
+        self.test_ranking = pd.DataFrame(data=rankings_test,columns=["alg1"])
 
         print("train instances", self.train_inst)
         print("test instances", self.test_inst)
@@ -59,13 +64,13 @@ class TestNeuralNetworkSynthetic(unittest.TestCase):
         model = nn.NeuralNetwork()
         rankings = util.ordering_to_ranking_list(self.train_ranking.values)
         model.fit(5, rankings,self.train_inst.values,self.train_performances.values,lambda_value=0,regression_loss="Squared")
-        # for index, row in self.test_inst.iterrows():
-        #     print("True Performances", self.test_performances.loc[index].values)
-        #     print("Predicted Performances", model.predict_performances(row.values))
-        #     print("\n")
-        #     print("True Ranking", self.test_ranking.loc[index].values)
-        #     print("Predicted Ranking", model.predict_ranking(row.values))
-        #     print("\n")
+        for index, row in self.test_inst.iterrows():
+            print("True Performances", self.test_performances.loc[index].values)
+            print("Predicted Performances", model.predict_performances(row.values))
+            print("\n")
+            print("True Ranking", self.test_ranking.loc[index].values)
+            print("Predicted Ranking", model.predict_ranking(row.values))
+            print("\n")
 
 
 if __name__ == "__main__":
--- Corras/Model/neural_net.py
+++ Corras/Model/neural_net.py
@@ -1,12 +1,13 @@
 import numpy as np
 np.random.seed(15)
 import pandas as pd
-import tensorflow as tf
-from tensorflow import keras
-import tensorflow.keras.backend as K
-from tensorflow.keras import layers
-from tensorflow.keras.losses import MSE
-from tensorflow.keras.optimizers import Adam
+# import tensorflow as tf
+import tensorflow_core as tf
+from tensorflow_core import keras
+from tensorflow_core.python.keras import layers
+from tensorflow_core.python.keras import backend as K
+from tensorflow_core.python.keras.optimizers import Adam
+from tensorflow_core.python.data import Dataset
 from Corras.Scenario.aslib_ranking_scenario import ASRankingScenario
 import logging
 
@@ -15,19 +16,20 @@ class NeuralNetwork:
     def __init__(self):
         self.network = None
         self.logger = logging.getLogger("CorrasNeuralNet")
+        K.set_floatx("float64")
 
 
-    def custom_optimizer(self, learning_rate=0.01):
+    # def custom_optimizer(self, learning_rate=0.01):
 
-        if self.network is None:
-            self.logger.error("No model build so far!")
-        predictions = self.network.outputs()
-        print(predictions)
-        optimizer = Adam(lr=learning_rate)
-        loss_val = 0
-        updates = optimizer.get_updates(self.network.trainable_weights, [], loss_val)
-        train = K.function([self.network.input, *predictions])
-        return train
+        # if self.network is None:
+        #     self.logger.error("No model build so far!")
+        # predictions = self.network.outputs()
+        # print(predictions)
+        # optimizer = Adam(lr=learning_rate)
+        # loss_val = 0
+        # updates = optimizer.get_updates(self.network.trainable_weights, [], loss_val)
+        # train = K.function([self.network.input, *predictions])
+        # return train
 
 
     def build_network(self, num_labels, num_features ):
@@ -38,9 +40,9 @@ class NeuralNetwork:
         output_layers = []
         for i in range(0, num_labels):
             output_layers.append(keras.layers.Dense(1, name="output_layer"+str(i))(hidden_layers))
-        return keras.Model(inputs=input_layer, outputs=output_layers)
+        return keras.Model(inputs=input_layer, outputs=output_layers, )
 
-    def fit(self, num_labels: int, rankings: np.ndarray, features: np.ndarray, performances : np.ndarray, lambda_value = 0.5, regression_loss="Absolute"):
+    def fit(self, num_labels: int, rankings: np.ndarray, features: np.ndarray, performances : np.ndarray, lambda_value = 0.5, regression_loss="Absolute", num_epochs=500, batch_size=32):
         """Fit the network to the given data.
 
         Arguments:
@@ -56,26 +58,60 @@ class NeuralNetwork:
         # add one column for bias
         num_features = features.shape[1]+1
         self.network = self.build_network(num_labels, num_features)
-        optimizer = tf.keras.optimizers.Adam()
 
-        def reg_squared_error(y_true, y_pred):
-            return tf.reduce_mean(tf.square(tf.subtract(y_true,tf.exp(y_pred))))
-
-        def reg_absolute_error(y_true, y_pred):
-            return tf.reduce_mean(tf.abs(tf.subtract(y_true,tf.exp(y_pred))))
-
-        # self.network.compile(loss=[reg_squared_error, reg_squared_error, reg_squared_error], optimizer=optimizer, metrics=["mse", "mae"])
         self.network._make_predict_function()
         self.network.summary()
         early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
-        # # add constant 1 for bias
+        # # add constant 1 for bias and create tf dataset
         feature_values = np.hstack((features,np.ones((features.shape[0],1))))
+        print(feature_values.shape)
+        print(performances.shape)
+        dataset = Dataset.from_tensor_slices((feature_values, performances))
+        dataset.batch(batch_size)
+        print("dataset", dataset)
+
+        print(self.network(feature_values[0, None]))
+        
+        # define custom loss function
+        def custom_loss(model, x, y):
+            y_ = model(x)
+            # compute MSE
+            loss = tf.reduce_mean(tf.square(tf.subtract(y,tf.exp(y_))))
+            print(y_)
+            # print(loss)
+            return loss
+        
+        l = custom_loss(self.network, feature_values, performances)
+
+        # define gradient of custom loss function
+        def grad(model, inputs, targets):
+            with tf.GradientTape() as tape:
+                loss_value = custom_loss(model, inputs, targets)
+            return loss_value, tape.gradient(loss_value, model.trainable_weights)
+        
+        # optimizer
+        optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+
+        train_loss_results = []
+
+        # training 
+        # epoch_accuracy = tf.keras.metrics.MSE()
+        for epoch in range(num_epochs):
+            # for x, y in zip(feature_values, performances):
+            for x,y in dataset:
+                # print(x,y)
+                loss_value, grads = grad(self.network,feature_values,performances)
+                # print(loss_value)
+                optimizer.apply_gradients(zip(grads, self.network.trainable_weights))
+
+            # epoch_accuracy(y,self.network(x))
+            # train_loss_results.append(epoch_loss)
+
+        # self.network.compile(loss=[custom_loss, custom_loss, custom_loss, custom_loss, custom_loss], optimizer=optimizer, metrics=["mse", "mae"])
         # self.network.fit(feature_values, [performances[:,0],performances[:,1],performances[:,2]], epochs = 10000, validation_split = 0.2, verbose = 0, callbacks=[early_stop])
 
-        predictions = self.network(feature_values[:2])
-        print(predictions)
-
+    
 
     def predict_performances(self, features: np.ndarray):
         """Predict a vector of performance values.
@@ -89,9 +125,9 @@ class NeuralNetwork:
         # add constant 1 for bias
         features = np.hstack((features, [1]))
         # keras expects a 2 dimensional input
-        features = np.expand_dims(features, axis=0)
+        # features = np.expand_dims(features, axis=0)
          # compute utility scores
-        utility_scores = np.exp(self.network.predict(features))
+        utility_scores = np.exp(self.network(features[:,None].T))
         # return np.reciprocal(utility_scores)
         return utility_scores
 
@@ -106,5 +142,5 @@ class NeuralNetwork:
         """
         # compute utility scores
         features = np.hstack((features, [1]))
-        utility_scores = np.exp(np.dot(self.weights, features))
+        utility_scores = np.exp(self.network(features[:,None].T))
         return np.argsort(np.argsort(utility_scores)[::-1]) + 1
