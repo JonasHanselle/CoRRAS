92f1d9d9d2cfd5d0cf2cc70c6a955055551fe861
Jonas Hanselle <jmhansel@mail.uni-paderborn.de>
2020-01-15 18:42:54 +0100

    Intermediate commit

--- lin_hinge_scaled_experiment.py
+++ lin_hinge_scaled_experiment.py
@@ -34,10 +34,10 @@ total_shards = int(sys.argv[1])
 shard_number = int(sys.argv[2])
 
 # DB data
-db_url = sys.argv[3]
-db_user = sys.argv[4]
-db_pw = urllib.parse.quote_plus(sys.argv[5])
-db_db = sys.argv[6]
+# db_url = sys.argv[3]
+# db_user = sys.argv[4]
+# db_pw = urllib.parse.quote_plus(sys.argv[5])
+# db_db = sys.argv[6]
 
 scenarios = ["MIP-2016", "SAT11-HAND", "SAT11-INDU", "SAT11-RAND", "CSP-2010"]
 # scenarios = ["MIP-2016"]
@@ -83,44 +83,44 @@ for scenario_name, lambda_value, epsilon_value, split, seed, use_quadratic_trans
 
     table_name = "linear-squared-hinge-new-parameters-" + scenario_name
 
-    engine = sql.create_engine("mysql://" + db_user + ":" + db_pw + "@" +
-                               db_url + "/" + db_db,
-                               echo=False)
-    connection = engine.connect()
-    if not engine.dialect.has_table(engine, table_name):
-        pass
-    else:
-        meta = MetaData(engine)
-        experiments = Table(table_name,
-                            meta,
-                            autoload=True,
-                            autoload_with=engine)
+    # engine = sql.create_engine("mysql://" + db_user + ":" + db_pw + "@" +
+    #                            db_url + "/" + db_db,
+    #                            echo=False)
+    # connection = engine.connect()
+    # if not engine.dialect.has_table(engine, table_name):
+    #     pass
+    # else:
+    #     meta = MetaData(engine)
+    #     experiments = Table(table_name,
+    #                         meta,
+    #                         autoload=True,
+    #                         autoload_with=engine)
 
-        slct = experiments.select(
-            and_(
-                experiments.columns["split"] == split,
-                experiments.columns["lambda"] == lambda_value,
-                experiments.columns["epsilon"] == epsilon_value,
-                experiments.columns["seed"] == seed,
-                experiments.columns["use_quadratic_transform"] ==
-                use_quadratic_transform,
-                experiments.columns["skip_censored"] == skip_censored,
-                experiments.columns["regulerization_param"] ==
-                regulerization_param,
-                experiments.columns["use_weighted_samples"] ==
-                use_weighted_samples,
-                experiments.columns["scale_target_to_unit_interval"] ==
-                scale_target_to_unit_interval)).limit(1)
-        rs = connection.execute(slct)
-        result = rs.first()
-        if result == None:
-            pass
-        else:
-            rs.close()
-            connection.close()
-            continue
-        rs.close()
-        connection.close()
+    #     slct = experiments.select(
+    #         and_(
+    #             experiments.columns["split"] == split,
+    #             experiments.columns["lambda"] == lambda_value,
+    #             experiments.columns["epsilon"] == epsilon_value,
+    #             experiments.columns["seed"] == seed,
+    #             experiments.columns["use_quadratic_transform"] ==
+    #             use_quadratic_transform,
+    #             experiments.columns["skip_censored"] == skip_censored,
+    #             experiments.columns["regulerization_param"] ==
+    #             regulerization_param,
+    #             experiments.columns["use_weighted_samples"] ==
+    #             use_weighted_samples,
+    #             experiments.columns["scale_target_to_unit_interval"] ==
+    #             scale_target_to_unit_interval)).limit(1)
+    #     rs = connection.execute(slct)
+    #     result = rs.first()
+    #     if result == None:
+    #         pass
+    #     else:
+    #         rs.close()
+    #         connection.close()
+    #         continue
+    #     rs.close()
+    #     connection.close()
     params_string = "-".join([
         scenario_name,
         str(lambda_value),
@@ -142,139 +142,11 @@ for scenario_name, lambda_value, epsilon_value, split, seed, use_quadratic_trans
     loss_filepath = loss_path + loss_filename
     exists = os.path.exists(filepath)
     result_data_corras = []
-    try:
-        scenario_path = "./aslib_data-aslib-v4.0/" + scenario_name
-        scenario = aslib_ranking_scenario.ASRankingScenario()
-        scenario.read_scenario(scenario_path)
-        if not exists:
-            performance_cols_corras = [
-                x + "_performance" for x in scenario.performance_data.columns
-            ]
-
-            result_columns_corras = [
-                "split", "problem_instance", "lambda", "epsilon", "seed",
-                "use_quadratic_transform", "use_max_inverse_transform",
-                "scale_target_to_unit_interval", "skip_censored",
-                "regulerization_param", "use_weighted_samples"
-            ]
-            result_columns_corras += performance_cols_corras
-
-            results_corras = pd.DataFrame(data=[],
-                                          columns=result_columns_corras)
-            results_corras.to_csv(filepath, index_label="id")
-
-        test_scenario, train_scenario = scenario.get_split(split)
-
-        train_performances = train_scenario.performance_data
-        train_features = train_scenario.feature_data
-        # preprocessing
-        imputer = SimpleImputer()
-        polytransform = PolynomialFeatures(2)
-        scaler = StandardScaler()
-
-        # Impute
-        train_features[train_features.columns] = imputer.fit_transform(
-            train_features[train_features.columns])
-
-        # Create polynomial features
-        if use_quadratic_transform:
-            quad_data = polytransform.fit_transform(train_features.to_numpy())
-            new_cols = polytransform.get_feature_names(train_features.columns)
-            train_features = pd.DataFrame(data=quad_data,
-                                          index=train_features.index,
-                                          columns=new_cols)
-
-        # Standardize
-        train_features[train_features.columns] = scaler.fit_transform(
-            train_features[train_features.columns])
-
-        # inst, perf, rank = util.construct_numpy_representation_with_pairs_of_rankings(
-        #     train_features, train_performances, max_pairs_per_instance=max_pairs_per_instance, seed=seed)
-
-        cutoff = scenario.algorithm_cutoff_time
-        par10 = cutoff * 10
-
-        perf = train_performances.to_numpy()
-
-        order = "asc"
-
-        if use_max_inverse_transform == "max_cutoff":
-            perf = perf.clip(0, cutoff)
-            perf = cutoff - perf
-            order = "desc"
-        elif use_max_inverse_transform == "max_par10":
-            perf = par10 - perf
-            order = "desc"
-
-        perf_max = 1
-
-        if scale_target_to_unit_interval:
-            perf_max = np.max(perf)
-            perf = perf / perf_max
-
-        train_performances = pd.DataFrame(data=perf,
-                                          index=train_performances.index,
-                                          columns=train_performances.columns)
-        print(order)
-        skip_value = None
-        if skip_censored:
-            if use_max_inverse_transform == "none":
-                skip_value = train_performances.to_numpy().max()
-            else:
-                skip_value = train_performances.to_numpy().min()
-        print("skip value", skip_value)
-        inst, perf, rank, sample_weights = util.construct_numpy_representation_with_ordered_pairs_of_rankings_and_features_and_weights(
-            train_features,
-            train_performances,
-            max_pairs_per_instance=max_pairs_per_instance,
-            seed=seed,
-            order=order,
-            skip_value=skip_value)
-
-        model = mode1 = lh.LinearHingeModel()
-        model.fit_np(len(scenario.algorithms),
-                     rank,
-                     inst,
-                     perf,
-                     lambda_value=lambda_value,
-                     epsilon_value=epsilon_value,
-                     regression_loss="Squared",
-                     maxiter=maxiter,
-                     print_output=False,
-                     log_losses=True,
-                     reg_param=regulerization_param, sample_weights=sample_weights)
-
-        for index, row in test_scenario.feature_data.iterrows():
-            row_values = row.to_numpy().reshape(1, -1)
-
-            # Impute
-            imputed_row = imputer.transform(row_values)
-
-            # Quadratic features
-            if use_quadratic_transform:
-                imputed_row = polytransform.transform(imputed_row)
-
-            # Standardize
-            scaled_row = scaler.transform(imputed_row).flatten()
-            predicted_ranking = model.predict_ranking(scaled_row)
-            predicted_performances = model.predict_performances(scaled_row)
-
-            if scale_target_to_unit_interval:
-                predicted_performances = perf_max * predicted_performances
-            if use_max_inverse_transform == "max_cutoff":
-                predicted_performances = cutoff - predicted_performances
-            elif use_max_inverse_transform == "max_par10":
-                predicted_performances = par10 - predicted_performances
-
-            result_data_corras.append([
-                split, index, lambda_value, epsilon_value, seed,
-                use_quadratic_transform, use_max_inverse_transform,
-                scale_target_to_unit_interval, skip_censored,
-                regulerization_param, use_weighted_samples,
-                *predicted_performances
-            ])
-            # scenario_name, lambda_value, split, seed, use_quadratic_transform, use_max_inverse_transform, scale_target_to_unit_interval
-
+    # try:
+    scenario_path = "./aslib_data-aslib-v4.0/" + scenario_name
+    scenario = aslib_ranking_scenario.ASRankingScenario()
+    scenario.read_scenario(scenario_path)
+    if not exists:
         performance_cols_corras = [
             x + "_performance" for x in scenario.performance_data.columns
         ]
@@ -286,19 +158,147 @@ for scenario_name, lambda_value, epsilon_value, split, seed, use_quadratic_trans
             "regulerization_param", "use_weighted_samples"
         ]
         result_columns_corras += performance_cols_corras
-        results_corras = pd.DataFrame(data=result_data_corras,
-                                      columns=result_columns_corras)
-        results_corras.to_csv(filepath,
-                              index_label="id",
-                              mode="a",
-                              header=False)
-        connection = engine.connect()
-        results_corras.to_sql(name=table_name,
-                              con=connection,
-                              if_exists="append")
-        connection.close()
-        model.save_loss_history(loss_filepath)
 
-    except Exception as exc:
-        print("Something went wrong during computation with parameters " +
-              params_string + " message: " + str(exc))
+        results_corras = pd.DataFrame(data=[],
+                                        columns=result_columns_corras)
+        results_corras.to_csv(filepath, index_label="id")
+
+    test_scenario, train_scenario = scenario.get_split(split)
+
+    train_performances = train_scenario.performance_data
+    train_features = train_scenario.feature_data
+    # preprocessing
+    imputer = SimpleImputer()
+    polytransform = PolynomialFeatures(2)
+    scaler = StandardScaler()
+
+    # Impute
+    train_features[train_features.columns] = imputer.fit_transform(
+        train_features[train_features.columns])
+
+    # Create polynomial features
+    if use_quadratic_transform:
+        quad_data = polytransform.fit_transform(train_features.to_numpy())
+        new_cols = polytransform.get_feature_names(train_features.columns)
+        train_features = pd.DataFrame(data=quad_data,
+                                        index=train_features.index,
+                                        columns=new_cols)
+
+    # Standardize
+    train_features[train_features.columns] = scaler.fit_transform(
+        train_features[train_features.columns])
+
+    # inst, perf, rank = util.construct_numpy_representation_with_pairs_of_rankings(
+    #     train_features, train_performances, max_pairs_per_instance=max_pairs_per_instance, seed=seed)
+
+    cutoff = scenario.algorithm_cutoff_time
+    par10 = cutoff * 10
+
+    perf = train_performances.to_numpy()
+
+    order = "asc"
+
+    if use_max_inverse_transform == "max_cutoff":
+        perf = perf.clip(0, cutoff)
+        perf = cutoff - perf
+        order = "desc"
+    elif use_max_inverse_transform == "max_par10":
+        perf = par10 - perf
+        order = "desc"
+
+    perf_max = 1
+
+    if scale_target_to_unit_interval:
+        perf_max = np.max(perf)
+        perf = perf / perf_max
+
+    train_performances = pd.DataFrame(data=perf,
+                                        index=train_performances.index,
+                                        columns=train_performances.columns)
+    print(order)
+    skip_value = None
+    if skip_censored:
+        if use_max_inverse_transform == "none":
+            skip_value = train_performances.to_numpy().max()
+        else:
+            skip_value = train_performances.to_numpy().min()
+    print("skip value", skip_value)
+    inst, perf, rank, sample_weights = util.construct_numpy_representation_with_ordered_pairs_of_rankings_and_features_and_weights(
+        train_features,
+        train_performances,
+        max_pairs_per_instance=max_pairs_per_instance,
+        seed=seed,
+        order=order,
+        skip_value=skip_value)
+
+    model = mode1 = lh.LinearHingeModel()
+    model.fit_np(len(scenario.algorithms),
+                    rank,
+                    inst,
+                    perf,
+                    lambda_value=lambda_value,
+                    epsilon_value=epsilon_value,
+                    regression_loss="Squared",
+                    maxiter=maxiter,
+                    print_output=False,
+                    log_losses=True,
+                    reg_param=regulerization_param, sample_weights=sample_weights)
+
+    for index, row in test_scenario.feature_data.iterrows():
+        row_values = row.to_numpy().reshape(1, -1)
+
+        # Impute
+        imputed_row = imputer.transform(row_values)
+
+        # Quadratic features
+        if use_quadratic_transform:
+            imputed_row = polytransform.transform(imputed_row)
+
+        # Standardize
+        scaled_row = scaler.transform(imputed_row).flatten()
+        predicted_ranking = model.predict_ranking(scaled_row)
+        predicted_performances = model.predict_performances(scaled_row)
+
+        if scale_target_to_unit_interval:
+            predicted_performances = perf_max * predicted_performances
+        if use_max_inverse_transform == "max_cutoff":
+            predicted_performances = cutoff - predicted_performances
+        elif use_max_inverse_transform == "max_par10":
+            predicted_performances = par10 - predicted_performances
+
+        result_data_corras.append([
+            split, index, lambda_value, epsilon_value, seed,
+            use_quadratic_transform, use_max_inverse_transform,
+            scale_target_to_unit_interval, skip_censored,
+            regulerization_param, use_weighted_samples,
+            *predicted_performances
+        ])
+        # scenario_name, lambda_value, split, seed, use_quadratic_transform, use_max_inverse_transform, scale_target_to_unit_interval
+
+    performance_cols_corras = [
+        x + "_performance" for x in scenario.performance_data.columns
+    ]
+
+    result_columns_corras = [
+        "split", "problem_instance", "lambda", "epsilon", "seed",
+        "use_quadratic_transform", "use_max_inverse_transform",
+        "scale_target_to_unit_interval", "skip_censored",
+        "regulerization_param", "use_weighted_samples"
+    ]
+    result_columns_corras += performance_cols_corras
+    results_corras = pd.DataFrame(data=result_data_corras,
+                                    columns=result_columns_corras)
+    # results_corras.to_csv(filepath,
+    #                       index_label="id",
+    #                       mode="a",
+    #                       header=False)
+    # connection = engine.connect()
+    # results_corras.to_sql(name=table_name,
+    #                       con=connection,
+    #                       if_exists="append")
+    # connection.close()
+    model.save_loss_history(loss_filepath)
+
+    # except Exception as exc:
+    #     print("Something went wrong during computation with parameters " +
+    #           params_string + " message: " + str(exc))
