c7d100265aa4338c216679e870a0dbce84c0b964
Jonas Manuel Hanselle <jmhansel@mail.uni-paderborn.de>
2020-03-02 11:08:53 +0100

    Work on plot

--- loss_plot_pl.py
+++ loss_plot_pl.py
@@ -1,4 +1,4 @@
-import os 
+import os
 
 import numpy as np
 import pandas as pd
@@ -8,21 +8,22 @@ from itertools import product
 # measures
 from scipy.stats import kendalltau, describe
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-from Corras.Evaluation.evaluation import ndcg_at_k, compute_relevance_scores_unit_interval 
+from Corras.Evaluation.evaluation import ndcg_at_k, compute_relevance_scores_unit_interval
 
 # plotting
 import matplotlib.pyplot as plt
+import matplotlib
 import seaborn as sns
 
 sns.set_style("whitegrid")
 
 scenario_names = ["SAT11-RAND", "MIP-2016", "CSP-2010", "SAT11-INDU"]
-scenario_names = ["SAT11-RAND"]
+scenario_names = ["SAT11-INDU"]
 scenario_path = "./aslib_data-aslib-v4.0/"
 result_path = "./losses-pl/"
 figures_path = "./figures_loss_hist_pl/"
-figures_path = "../Masters_Thesis/Thesis/latex-thesis-template/gfx/plots/pl/losses"
-lambda_values = [0.4]
+figures_path = "../Masters_Thesis/masters-thesis/gfx/plots/pl/losses"
+lambda_values = [0.6]
 # epsilon_values = [0.2,0.4,0.6,0.8,1]
 split = 4
 seed = 15
@@ -41,29 +42,31 @@ use_max_inverse_transform_values = ["max_cutoff"]
 scale_target_to_unit_interval_values = [True]
 
 splits = [4]
-params = [scenario_names, lambda_values, splits, seeds, use_quadratic_transform_values, use_max_inverse_transform_values, scale_target_to_unit_interval_values]
+params = [scenario_names, lambda_values, splits, seeds, use_quadratic_transform_values,
+          use_max_inverse_transform_values, scale_target_to_unit_interval_values]
 
 
 param_product = list(product(*params))
 
 # /home/jonas/Documents/CoRRAS/loss-hists/loss-hist-hinge-SAT11-RAND-1.0-0.6-4-15.csv
+fig, axes = plt.subplots(1, 2)
 
-for scenario_name, lambda_value, split, seed, use_quadratic_transform, use_max_inverse_transform, scale_target_to_unit_interval in param_product:
+for index, (scenario_name, lambda_value, split, seed, use_quadratic_transform, use_max_inverse_transform, scale_target_to_unit_interval) in enumerate(param_product):
     params_string = "-".join([scenario_name,
                               str(lambda_value), str(split), str(seed), str(use_quadratic_transform), str(use_max_inverse_transform), str(scale_target_to_unit_interval)])
 
     filename = "pl_log_linear" + "-" + params_string + ".csv"
     loss_filename = "pl_log_linear" + "-" + params_string + "-losses.csv"
     filepath = result_path + filename
-    loss_filepath = result_path + loss_filename    
-    figure_file = params_string.replace(".","_") + "-losses" + ".pdf"
+    loss_filepath = result_path + loss_filename
+    figure_file = params_string.replace(".", "_") + "-losses" + ".pdf"
     df = None
     if not os.path.isfile(loss_filepath):
         print("File for " + params_string + " not found!")
         continue
     df = pd.read_csv(loss_filepath)
     print(df)
-    df = df.rename(columns={"iter":"iteration"})
+    df = df.rename(columns={"iter": "iteration"})
     # df = df.rename(columns={"NLL":"PL-NLL"})
     df["$\lambda$ NLL"] = lambda_value * df["NLL"]
     df["$(1 - \lambda)$ MSE"] = (1 - lambda_value) * df["MSE"]
@@ -75,11 +78,22 @@ for scenario_name, lambda_value, split, seed, use_quadratic_transform, use_max_i
     text += "seed = " + str(seed) + ", "
     text += "quadratic transform: " + str(use_quadratic_transform) + ",\n"
     text += "max inverse transform: " + str(use_max_inverse_transform) + ", "
-    text += "scale target to unit interval: " + str(scale_target_to_unit_interval) + ", "
+    text += "scale target to unit interval: " + \
+        str(scale_target_to_unit_interval) + ", "
 
-    plt.clf()
+    # plt.clf()
     # plt.tight_layout()
-    plt.annotate(text,(0,0), (0,-40), xycoords="axes fraction", textcoords="offset points", va="top")
-    lp = sns.lineplot(x="iteration", y="value", hue="variable", data=df)
-    plt.title(scenario_name)
-    plt.savefig(figures_path+figure_file, bbox_inches="tight")
+    # plt.annotate(text,(0,0), (0,-40), xycoords="axes fraction", textcoords="offset points", va="top")
+    lp = sns.lineplot(x="iteration", y="value",
+                      hue="variable", data=df, ax=axes[index], legend=None)
+    axes[index].set_xlabel("Iteration")
+    axes[index].set_ylabel("Value")
+    axes[index].set_title(scenario_name)
+    # if index == 1:
+labels=["MSE", "NLL", "$\\lambda$NLL", "$(1-\\lambda)$MSE", "Total Loss"]
+plt.annotate("",(0,0), (0,-40), xycoords="axes fraction", textcoords="offset points", va="top")
+fig.set_size_inches(8, 3.3)
+legend = fig.legend(list(axes), labels=labels, loc="lower center", ncol=len(
+    labels), bbox_to_anchor=(0.45, -0.00))
+plt.subplots_adjust(bottom=0.25)
+plt.savefig(figures_path+figure_file, bbox_inches="tight")
